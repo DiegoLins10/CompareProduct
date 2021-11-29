e8d965eabc5b55e77d24e0c3667b91611ba016b2
DiegoLins10 <diegofernandeslins@gmail.com>
2021-11-28 21:17:36 -0300

    Add project files.

---
+++ CompareProduct/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace CompareProduct.Entities
+{
+    class Product  : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Price: " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        /*
+         * Interface compareTo que ordena pelo nome
+         */
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ CompareProduct/Program.cs
@@ -0,0 +1,24 @@
+﻿using CompareProduct.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CompareProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 900));
+            list.Add(new Product("Celular", 1000));
+            list.Add(new Product("Mouse", 200));
+
+            list.Sort();
+
+            foreach(var line in list)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
